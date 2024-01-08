07c36d19777dd51b56d734ebdae50b3ed360d46d
Keith Hart aka ownimage <keith@ownimage.co.uk>
2024-01-08 16:24:09 +0000

    Added test

--- 05 Lightroom AI Description Generation/xmp_processor.py
+++ 05 Lightroom AI Description Generation/xmp_processor.py
@@ -1,9 +1,6 @@
 # https://www.geeksforgeeks.org/reading-and-writing-xml-files-in-python/
-import sys
-import xml.etree.ElementTree as ET
 
-filename = sys.argv[1]
-print(f'filename = {filename}')
+import xml.etree.ElementTree as ET
 
 namespace = {
     'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
@@ -11,14 +8,14 @@ namespace = {
 }
 
 
-def expect_one_in_list(single_list, name):
-    o = expect_one_or_none_in_list(single_list, name)
+def __expect_one_in_list(single_list, name):
+    o = __expect_one_or_none_in_list(single_list, name)
     if o is not None:
         return o
     raise Exception(f'{name} element not found.')
 
 
-def expect_one_or_none_in_list(single_list, name):
+def __expect_one_or_none_in_list(single_list, name):
     if len(single_list) == 0:
         return None
     if len(single_list) == 1:
@@ -28,29 +25,29 @@ def expect_one_or_none_in_list(single_list, name):
 
 def get_user_comment(tree):
     es = tree.findall('./rdf:RDF/rdf:Description/exif:UserComment', namespace)
-    return expect_one_or_none_in_list(es, 'UserComment')
+    return __expect_one_or_none_in_list(es, 'UserComment')
 
 
-def get_Description(tree):
+def __get_Description(tree):
     d = tree.findall('./rdf:RDF/rdf:Description', namespace)
-    return expect_one_in_list(d, 'Description')
+    return __expect_one_in_list(d, 'Description')
 
 
-def get_or_create_user_comment(tree):
+def __get_or_create_user_comment(tree):
     user_comment = get_user_comment(tree)
     if user_comment is None:
-        description = get_Description(tree)
+        description = __get_Description(tree)
         user_comment = ET.SubElement(description, 'exif:UserComment')
     return user_comment
 
 
-def register_all_namespaces(filename):
+def __register_all_namespaces(filename):
     namespaces = dict([node for _, node in ET.iterparse(filename, events=['start-ns'])])
     for ns in namespaces:
         ET.register_namespace(ns, namespaces[ns])
 
 
-def replace_user_comment(element, text):
+def __replace_user_comment(element, text):
     alt = element.find('rdf:Alt', namespace)
     if alt is None:
         alt = ET.SubElement(element, 'rdf:Alt')
@@ -60,11 +57,12 @@ def replace_user_comment(element, text):
     li.text = text
 
 
-register_all_namespaces(filename)
-tree = ET.parse(filename)
-root = tree.getroot()
-user_comment = get_or_create_user_comment(tree)
-replace_user_comment(user_comment, 'Hello Mum yy')
-ET.indent(tree, space=" ", level=0)
-tree.write('out.xml')
+def process(filename, text):
+    __register_all_namespaces(filename)
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    user_comment = __get_or_create_user_comment(tree)
+    __replace_user_comment(user_comment, text)
+    ET.indent(tree, space=" ", level=0)
+    tree.write(filename)
 
